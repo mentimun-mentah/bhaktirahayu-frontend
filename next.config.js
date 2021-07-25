a2917901845355d5374ba7b91e912c9c632b10d7
paulussimanjuntak <paulusbsimanjuntak@gmail.com>
2021-07-25 22:44:24 +0800

    update package and next.config.js and .env

--- next.config.js
+++ next.config.js
@@ -1,14 +1,23 @@
 const withAntdLess = require('next-plugin-antd-less')
 const { theme } = require('./styles/theme')
+const imageDomains = process.env.NEXT_PUBLIC_IMAGE_HOSTNAME.split(" ")
 
 module.exports = withAntdLess({
   modifyVars: theme,
   reactStrictMode: false,
   images: {
     deviceSizes: [320, 420, 768, 1024, 1200],
-    domains: ['ecs7.tokopedia.net', 'os.alipayobjects.com'],
+    domains: imageDomains,
     path: '/_next/image',
     loader: 'default',
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    API_URL: process.env.NEXT_PUBLIC_BACKEND_URL
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    API_URL: process.env.NEXT_PUBLIC_API_URL
   },
 
   webpack(config) {
